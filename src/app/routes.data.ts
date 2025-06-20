bb13257e0d0289c4f60f1f5acf31cc68f3f44b98
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-06-20 15:35:35 +0300

    feat: added more example routes to the ROUTES mock data

--- src/app/routes.data.ts
+++ src/app/routes.data.ts
@@ -49,4 +49,25 @@ export const ROUTES: Route[] = [
     gateway: '193.0.161.1',
     interface: 'Подключение Ethernet',
   },
+  {
+    uuid: '7',
+    address: '172.16.0.0/16',
+    mask: '255.255.0.0',
+    gateway: '0.0.0.0',
+    interface: 'Домашняя сеть',
+  },
+  {
+    uuid: '8',
+    address: '100.64.0.0/10',
+    mask: '255.192.0.0',
+    gateway: '10.13.0.1',
+    interface: 'Гостевая сеть',
+  },
+  {
+    uuid: '9',
+    address: '127.0.0.1/32',
+    mask: '255.255.255.255',
+    gateway: '0.0.0.0',
+    interface: 'Гостевая сеть',
+  },
 ];
