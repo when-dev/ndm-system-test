fd4b7538042cf73c2d53d78699c06c820923fa6e
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-06-20 15:02:26 +0300

    feat: added basic sorting to routes table by column

--- src/app/routes-table/routes-table.component.ts
+++ src/app/routes-table/routes-table.component.ts
@@ -10,5 +10,21 @@ import { ROUTES, Route } from '../routes.data';
   styleUrls: ['./routes-table.component.css'],
 })
 export class RoutesTableComponent {
-  routes: Route[] = ROUTES;
+  routes: Route[] = [...ROUTES];
+  sortColumn: keyof Route = 'address';
+  sortDirection: 1 | -1 = 1;
+
+  sort(column: keyof Route) {
+    if (this.sortColumn === column) {
+      this.sortDirection = -this.sortDirection as 1 | -1;
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 1;
+    }
+    this.routes.sort((a, b) => {
+      if (a[column] < b[column]) return -1 * this.sortDirection;
+      if (a[column] > b[column]) return 1 * this.sortDirection;
+      return 0;
+    })
+  }
 }
